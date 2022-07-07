889043c3295d30e558407b119cfb0ea3b761ab44
InSeong-So <goflvhxj2547@gmail.com>
2022-07-07 21:21:21 +0900

    🔬Chore: 프로젝트 셋업 브런치 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Posts } from './Posts';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import './App.css';
@@ -9,8 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <h1>Blog Posts</h1>
-        <Posts />
+
       </div>
       <ReactQueryDevtools />
     </QueryClientProvider>
