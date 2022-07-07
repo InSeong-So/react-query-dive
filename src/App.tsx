949a5798e28b54ebf7797fd61c93027946e5c6c5
InSeong-So <goflvhxj2547@gmail.com>
2022-07-07 21:18:44 +0900

    ✒️Feat: 무한 스크롤 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Posts } from './Posts';
+import './App.css';
+import { InfinitePeople } from './people/InfinitePeople';
+import { InfiniteSpecies } from './species/InfiniteSpecies';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import './App.css';
 
 const queryClient = new QueryClient();
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <h1>Blog Posts</h1>
-        <Posts />
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
       </div>
       <ReactQueryDevtools />
     </QueryClientProvider>
---
+++ src/people/InfinitePeople.tsx
@@ -0,0 +1,21 @@
+import InfiniteScroll from 'react-infinite-scroller';
+import { useInfiniteQuery } from 'react-query';
+import { Person } from './Person';
+
+const initialUrl = 'https://swapi.dev/api/people/';
+const fetchUrl = async (url: string) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+export function InfinitePeople() {
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    'sw-people',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: lastPage => lastPage.next || undefined,
+    },
+  );
+
+  return <InfiniteScroll loadMore={() => console.log()} />;
+}
